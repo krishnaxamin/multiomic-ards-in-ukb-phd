dade3cc26012dc448de2bf0dcf246c9fc75d4d09
Krishna <krishna.amin@qmul.ac.uk>
2026-09-10 10:37:17 +0100

    fix: correct paths

--- ch2_genomics/2.1 covariates/generate_genetic_principal_components.sh
+++ ch2_genomics/2.1 covariates/generate_genetic_principal_components.sh
@@ -12,35 +12,35 @@
 
 module load plink/1.9-170906;
 
-mapfile -t disease_fields < ../pan_ukbb_eur_qu10_50_disease_fields.txt;
+mapfile -t disease_fields < ~/data/internal/phenotype_coding/pan_ukbb_eur_qu10_50_disease_fields.txt;
 disease_field=${disease_fields[${SGE_TASK_ID}]};
 
-mkdir /data/scratch/bty207/ld_pruning/${disease_field};
+mkdir ~/data/internal/genomics/ld_pruning/${disease_field};
 
 # LD pruning
 plink \
---bfile /data/WHRI-Phenogenomics/krishna/ukb_genotype_data/ukb22418_autosomes-cX_b0_v2 \
---exclude variants_to_exclude/${disease_field}_autosomes-cX-vars_to_exclude.tsv \
---keep pan-ukbb-eur_eids.tsv \
---remove snp_qc_pipeline/step4_high_missingness_samples_panukbb_eur.tsv \
+--bfile ~/data/external/genomics/ukb_genotype_data/ukb22418_autosomes-cX_b0_v2  \
+--exclude "~/ch1_ard_discovery/1.2 cohort_data/1.2.1 qc/genomics/variants_to_exclude/${disease}_autosomes-cX-vars_to_exclude.tsv" \
+--keep ~/data/internal/cohort_eids/genomic_pan-ukbb-eur_eids.tsv \
+--remove "~/ch1_ard_discovery/1.2 cohort_data/1.2.1 qc/genomics/step4_high_missingness_samples_panukbb_eur.tsv" \
 --indep-pairwise 500 1 0.2 \
 --ld-xchr 1 \
 --threads $NSLOTS \
---out /data/scratch/bty207/ld_pruning/${disease_field}/${disease_field}_ld_pruning;
+--out ~/data/internal/genomics/ld_pruning/${disease_field}/${disease_field}_ld_pruning;
 
 module unload plink/1.9-170906;
 
 module load plink;
 
-mkdir /data/scratch/bty207/pca/${disease_field};
+mkdir ~/data/internal/genomics/pca/${disease_field};
 
 # PCA
 plink2 \
---bfile /data/WHRI-Phenogenomics/krishna/ukb_genotype_data/ukb22418_autosomes-cX_b0_v2 \
+--bfile ~/data/external/genomics/ukb_genotype_data/ukb22418_autosomes-cX_b0_v2 \
 --not-chr X \
---extract /data/scratch/bty207/ld_pruning/${disease_field}/${disease_field}_ld_pruning.prune.in \
---keep pan-ukbb-eur_eids.tsv \
---remove snp_qc_pipeline/step4_high_missingness_samples_panukbb_eur.tsv \
+--extract ~/data/internal/genomics/ld_pruning/${disease_field}/${disease_field}_ld_pruning.prune.in \
+--keep ~/data/internal/cohort_eids/genomic_pan-ukbb-eur_eids.tsv \
+--remove "~/ch1_ard_discovery/1.2 cohort_data/1.2.1 qc/genomics/step4_high_missingness_samples_panukbb_eur.tsv" \
 --pca allele-wts 20 approx vzs \
 --threads $NSLOTS \
---out /data/scratch/bty207/pca/${disease_field}/${disease_field}_pca
+--out ~/data/internal/genomics/pca/${disease_field}/${disease_field}_pca
